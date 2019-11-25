54473103d19bfb80c600ac77a7f3ddc47d2d5bfc
RudeFromwood <dubois.ruddy@carrefour.cegepvicto.ca>
2019-11-24 22:48:45 -0500

    yeet

---
+++ DesignWeb/ruddyduboisexamjs/js/script.js
@@ -0,0 +1,88 @@
+$("#i1").hide();
+$("#i2").hide();
+$("#i3").hide();
+$("#i4").hide();
+$("#i5").hide();
+$("#i6").hide();
+$("#i7").hide();
+$("#i8").hide();
+$("#i9").hide();
+$("#i10").hide();
+$("#i11").hide();
+$("#i12").hide();
+$("#i13").hide();
+
+
+
+$('#enter').click(function(){
+
+    $("#i1").hide();
+    $("#i2").hide();
+    $("#i3").hide();
+    $("#i4").hide();
+    $("#i5").hide();
+    $("#i6").hide();
+    $("#i7").hide();
+    $("#i8").hide();
+    $("#i9").hide();
+    $("#i10").hide();
+    $("#i11").hide();
+    $("#i12").hide();
+
+
+    text = $('#text1').val();
+
+
+    if(text.length >= 3){
+        $("#i1").show();
+        $("#i2").show();
+        $("#i3").show();
+    }
+    if(text.length >= 3){
+        $("#i3").show();
+    }
+    if(text.length >= 4){
+        $("#i4").show();
+    }
+    if(text.length >= 5){
+        $("#i5").show();
+    }
+    if(text.length >= 6){
+        $("#i6").show();
+    }
+    if(text.length >= 7){
+        $("#i7").show();
+    }
+    if(text.length >= 8){
+        $("#i8").show();
+    }
+    if(text.length >= 9){
+        $("#i9").show();
+    }
+    if(text.length >= 10){
+        $("#i10").show();
+    }
+    if(text.length >= 11){
+        $("#i11").show();
+    }
+    if(text.length >= 12){
+        $("#i12").show();
+    }
+
+
+    $("#i1").attr("src","img/letters/"+text[0]+"/"+text[0]+"1.jpg");
+    $("#i2").attr("src","img/letters/"+text[1]+"/"+text[1]+"1.jpg");
+    $("#i3").attr("src","img/letters/"+text[2]+"/"+text[2]+"1.jpg");
+    $("#i4").attr("src","img/letters/"+text[3]+"/"+text[3]+"1.jpg");
+    $("#i5").attr("src","img/letters/"+text[4]+"/"+text[4]+"1.jpg");
+    $("#i6").attr("src","img/letters/"+text[5]+"/"+text[5]+"1.jpg");
+    $("#i7").attr("src","img/letters/"+text[6]+"/"+text[6]+"1.jpg");
+    $("#i8").attr("src","img/letters/"+text[7]+"/"+text[7]+"1.jpg");
+    $("#i9").attr("src","img/letters/"+text[8]+"/"+text[8]+"1.jpg");
+    $("#i10").attr("src","img/letters/"+text[9]+"/"+text[9]+"1.jpg");
+    $("#i11").attr("src","img/letters/"+text[10]+"/"+text[10]+"1.jpg");
+    $("#i12").attr("src","img/letters/"+text[11]+"/"+text[11]+"1.jpg");
+
+});
+
+text = [0];
--- DesignWeb/ruddyhub/js/gacha.js
+++ DesignWeb/ruddyhub/js/gacha.js
@@ -10,7 +10,11 @@ var pull9;
 var pull10;
 var pull11;
 
+var rarity = ["ur","sr","sr","sr","r","r","r","r","r","r","r","r","r","r","r","r","r","r","r","r"];
+
 var card = ["1","2","3","4","5","6","7","8","9"];
+
+var box = 2;
 
 pull1 = (Math.floor(Math.random() * card.length)) + 1;
 pull2 = (Math.floor(Math.random() * card.length)) + 1;
@@ -24,20 +28,32 @@ pull9 = (Math.floor(Math.random() * card.length)) + 1;
 pull10 = (Math.floor(Math.random() * card.length)) + 1;
 pull11 = (Math.floor(Math.random() * card.length)) + 1;
 
+rar1 = (Math.floor(Math.random() * rarity.length));
+rar2 = (Math.floor(Math.random() * rarity.length));
+rar3 = (Math.floor(Math.random() * rarity.length));
+rar4 = (Math.floor(Math.random() * rarity.length));
+rar5 = (Math.floor(Math.random() * rarity.length));
+rar6 = (Math.floor(Math.random() * rarity.length));
+rar7 = (Math.floor(Math.random() * rarity.length));
+rar8 = (Math.floor(Math.random() * rarity.length));
+rar9 = (Math.floor(Math.random() * rarity.length));
+rar10 = (Math.floor(Math.random() * rarity.length));
+rar11 = (Math.floor(Math.random() * rarity.length));
 
+console.log(rar1);
 
 $("#pull").click(function(){
-    $("#pull1").attr("src","img/gacha/1/"+pull1+".png");
-    $("#pull2").attr("src","img/gacha/1/"+pull2+".png");
-    $("#pull3").attr("src","img/gacha/1/"+pull3+".png");
-    $("#pull4").attr("src","img/gacha/1/"+pull4+".png");
-    $("#pull5").attr("src","img/gacha/1/"+pull5+".png");
-    $("#pull6").attr("src","img/gacha/1/"+pull6+".png");
-    $("#pull7").attr("src","img/gacha/1/"+pull7+".png");
-    $("#pull8").attr("src","img/gacha/1/"+pull8+".png");
-    $("#pull9").attr("src","img/gacha/1/"+pull9+".png");
-    $("#pull10").attr("src","img/gacha/1/"+pull10+".png");
-    $("#pull11").attr("src","img/gacha/1/"+pull11+".png");
+    $("#pull1").attr("src","img/gacha/"+box+"/"+rarity[rar1]+"/"+pull1+".png");
+    $("#pull2").attr("src","img/gacha/"+box+"/"+rarity[rar2]+"/"+pull2+".png");
+    $("#pull3").attr("src","img/gacha/"+box+"/"+rarity[rar3]+"/"+pull3+".png");
+    $("#pull4").attr("src","img/gacha/"+box+"/"+rarity[rar4]+"/"+pull4+".png");
+    $("#pull5").attr("src","img/gacha/"+box+"/"+rarity[rar5]+"/"+pull5+".png");
+    $("#pull6").attr("src","img/gacha/"+box+"/"+rarity[rar6]+"/"+pull6+".png");
+    $("#pull7").attr("src","img/gacha/"+box+"/"+rarity[rar7]+"/"+pull7+".png");
+    $("#pull8").attr("src","img/gacha/"+box+"/"+rarity[rar8]+"/"+pull8+".png");
+    $("#pull9").attr("src","img/gacha/"+box+"/"+rarity[rar9]+"/"+pull9+".png");
+    $("#pull10").attr("src","img/gacha/"+box+"/"+rarity[rar10]+"/"+pull10+".png");
+    $("#pull11").attr("src","img/gacha/"+box+"/"+rarity[rar11]+"/"+pull11+".png");
 });
 
 
